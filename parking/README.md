a89fac143c0c177785e1c143eddb2e2fc1936acd
Gerrit Schrader <gerrit.schrader@gmail.com>
2020-11-25 07:46:08 +0100

    'getRotationAngle' added

--- parking/README.md
+++ parking/README.md
@@ -1,3 +1,4 @@
 # README
 
-- [How to Center Anything with CSS - Align a Div, Text, and More](https://www.freecodecamp.org/news/how-to-center-anything-with-css-align-a-div-text-and-more/)
+1. [Maintain the aspect ratio of a div with CSS](https://stackoverflow.com/questions/1495407/maintain-the-aspect-ratio-of-a-div-with-css)
+1. [How to Center Anything with CSS - Align a Div, Text, and More](https://www.freecodecamp.org/news/how-to-center-anything-with-css-align-a-div-text-and-more/)
--- parking/src/event.js
+++ parking/src/event.js
@@ -1,19 +1,45 @@
-function wheel(e) { // WheelEvent
-  console.log(`dy=${e.deltaY}`)
-}
-
-function move(e) {  // MouseEvent
-  console.log(`x=${e.clientX}, y=${e.clientY}`)
-}
-
-function rotateY(e) {
-  var deg = deg + 10;
-  if (deg == 360) {
-    deg = 0;
+function getRotationAngle(el) {
+  const style = window.getComputedStyle(el, null);
+  // console.log(style)
+  const matrix = style.getPropertyValue("transform");
+  console.log(matrix);
+  let values = matrix.split('(')[1];
+  values = values.split(')')[0];
+  values = values.split(',');
+  // console.log(values);
+  const a = values[0];
+  const b = values[1];
+  const c = values[2];
+  const d = values[3];
+  // console.log(`a=${a}, b=${b}, c=${c}, d=${d}`)
+  const radians = Math.atan2(b, a);
+  if (radians < 0) {
+    radians += (2 * Math.PI);
   }
-  level = document.querySelector(".level");
-  div.style.transform = 'rotateY(' + deg + 'deg)';
+  const angle = Math.round(radians * (180 / Math.PI));
+  console.log(angle)
+  return angle
 }
 
-document.querySelector(".scene").onwheel = wheel;
-document.querySelector(".scene").onmousemove = move;
+var angle
+
+function rotateX(ev) {
+  angle = (angle) ? angle + 20 : 20;
+  console.log(angle)
+  rectangle = document.querySelector(".rectangle");
+  // angle = getRotationAngle(rectangle) + 20;
+  rectangle.style.transform = `rotateX(${angle}deg)`;
+}
+
+function wheel(ev) { // WheelEvent
+  console.log(`dy=${ev.deltaY}`)
+}
+
+function move(event) {  // MouseEvent
+  console.log(`x=${event.clientX}, y=${event.clientY}`)
+}
+
+document.querySelector(".scene").onclick = rotateX;
+// document.querySelector(".rectangle").onwheel = wheel;
+// document.querySelector(".rectangle").onmousemove = move;
+
