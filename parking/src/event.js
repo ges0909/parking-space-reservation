dfcd01ee95adcd1d61510bbcfdfd7725400dadd7
Gerrit Schrader <gerrit.schrader@gmail.com>
2020-11-26 13:50:48 +0100

    mouse control for translating

--- parking/src/event.js
+++ parking/src/event.js
@@ -1,25 +1,59 @@
-var scale = 1
-var angleX = 0
-var angleY = 0
+var x = 0;
+var y = 0;
+var angleX = 0;
+var angleY = 0;
+var scale = 1;
+var leftButton = false;
+var rightButton = false;
 
 const rectangle = document.querySelector(".rectangle");
+rectangle.style.transform = `translateX(${x}px) translateY(${y}px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})`;
 
-function scale2d(ev) { // WheelEvent
+function mousemove(ev) { // MouseEvent
+  if (leftButton) {
+    if (Math.abs(ev.movementX) > Math.abs(ev.movementY)) {
+      x += ev.movementX;
+    }
+    else {
+      y += ev.movementY;
+    }
+    rectangle.style.transform = `translateX(${x}px) translateY(${y}px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})`;
+  }
+  else if (rightButton) {
+    if (Math.abs(ev.movementX) > Math.abs(ev.movementY)) {
+      angleY += (ev.movementX < 0 ? -1 : 1);
+    } else {
+      angleX += (ev.movementY < 0 ? -1 : 1);
+    }
+    rectangle.style.transform = `translateX(${x}px) translateY(${y}px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})`;
+  }
+}
+
+function mousedown(ev) {
+  if (ev.button == 0) {
+    leftButton = true;
+  }
+  else if (ev.button == 2) {
+    rightButton = true;
+  }
+}
+
+function mouseup(ev) {
+  if (ev.button == 0) {
+    leftButton = false;
+  }
+  else if (ev.button == 2) {
+    rightButton = false;
+  }
+}
+
+function wheel(ev) { // WheelEvent
   scale = (ev.deltaY < 0) ? scale - 0.05 : scale + 0.05;
   rectangle.style.transform = `scale(${scale}) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
 }
 
-function rotateXY(ev) { // MouseEvent
-  angleY += (ev.movementX / 2);
-  angleX += (ev.movementY / 2);
-  rectangle.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(${scale})`;
-}
+document.querySelector(".scene").onmousemove = mousemove;
+document.querySelector(".scene").onmousedown = mousedown;
+document.querySelector(".scene").onmouseup = mouseup;
+document.querySelector(".scene").onwheel = wheel;
 
-function move(ev) {  // MouseEvent
-  // console.log(ev)
-  // console.log(`x=${ev.x}, y=${ev.y}`)
-  console.log(`x=${ev.movementX}, y=${ev.movementY}`)
-}
-
-document.querySelector(".scene").onwheel = scale2d;
-document.querySelector(".scene").onmousemove = rotateXY;
